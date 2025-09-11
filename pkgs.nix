d5c3785c7c8c0dbeba1fe07a8f9217edcd5cfa9b
amida <aaaamida6747@gmail.com>
2025-09-11 23:31:25 +0800

    update: pkgs

--- pkgs.nix
+++ pkgs.nix
@@ -1,11 +1,13 @@
 { config, pkgs, inputs, ... }:
-
+let
+        zen-browser = (builtins.getFlake https://github.com/0xc000022070/zen-browser-flake/tarball/master).packages.x86_64-linux.default;
+in
 {
         home.packages = with pkgs; [
+                zen-browser
                 lazygit
                 gh
-                btop
-		fastfetch
+                fastfetch
                 hyfetch
                 rustup
                 clang
@@ -23,5 +25,6 @@
                 fzf
                 bat
                 eza
+                nerd-fonts.iosevka-term-slab
         ];
 }
