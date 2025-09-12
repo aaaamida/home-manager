e9ef0d7aff1c4f350168c5f0448c39a4d7d9f6f5
amida <aaaamida6747@gmail.com>
2025-09-13 02:08:11 +0800

    update: tidying up

--- pkgs.nix
+++ pkgs.nix
@@ -1,30 +1,44 @@
 { config, pkgs, inputs, ... }:
 let
         zen-browser = (builtins.getFlake https://github.com/0xc000022070/zen-browser-flake/tarball/master).packages.x86_64-linux.default;
+        freedownloadmanager = import (pkgs.fetchFromGitHub {
+                owner = "aaaamida";
+                repo = "nix-fdm";
+                rev = "master";
+                hash = "sha256-QYIM2bcK9WOhvbjZ8REHAdwbWLTz9fEh+34ssPM3j4Q=";
+        }) {};
 in
 {
         home.packages = with pkgs; [
+                # GUI programs
                 zen-browser
+                discord
+                youtube-music
+                freedownloadmanager
+
+                # CLI utils
                 lazygit
+                fzf
                 gh
                 fastfetch
                 hyfetch
+                yt-dlp
+                betterdiscordctl
+                appimage-run
+                zoxide
+                bat
+                eza
+                fd
+
+                # PL tools
                 rustup
                 clang
                 python314
                 libgcc
-                discord
-                youtube-music
-                yt-dlp
-                betterdiscordctl
-                cloudflare-warp
-                appimage-run
                 gnumake
                 cmake
-                zoxide
-                fzf
-                bat
-                eza
+
+                # misc
                 nerd-fonts.iosevka-term-slab
         ];
 }
