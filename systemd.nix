488d72714e113e705805154d96a5825313c63ff2
amida <aaaamida6747@gmail.com>
2025-09-16 09:58:07 +0800

    create: systemd user services

---
+++ systemd.nix
@@ -0,0 +1,14 @@
+{}:
+let
+        wallpaper = /home/amida/Pictures/gyr_up.mp4;
+in
+{
+        systemd.user.services.mpvpaper-autostart = {
+                Unit.Description = "Autostart mpvpaper.";
+                Install.WantedBy = [ "default.target" ];
+                Service.ExecStart = "${pkgs.writeShellScript "mpvpaper-autostart" ''
+                        #!/run/current-system/sw/bin/bash
+                        ${pkgs.mpvpaper}/bin/mpvpaper -o "no-audio loop" eDP-1 ${wallpaper}
+                ''}";
+        };
+}
