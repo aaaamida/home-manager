d96e6133ee3d27f8ffea8e3529b915f5f9b24e11
amida <aaaamida6747@gmail.com>
2025-09-16 14:31:49 +0800

    update: disable autostart

--- systemd.nix
+++ systemd.nix
@@ -1,20 +1,21 @@
-{}:
-let
-        wallpaper = /home/amida/Pictures/gyr_up.mp4;
-in
+{ config, pkgs, ... }:
+
 {
-        systemd.user.services.mpvpaper-autostart = {
-                Unit = {
-                        Description = "Autostart mpvpaper.";
-                };
-                Install = {
-                        WantedBy = [ "default.target" ];
-                };
-                Service = {
-                        ExecStart = "${pkgs.writeShellScript "mpvpaper-autostart" ''
-                                #!/run/current-system/sw/bin/bash
-                                ${pkgs.mpvpaper}/bin/mpvpaper -o "no-audio loop" eDP-1 ${wallpaper}
-                        ''}";
-                };
-        };
+        # systemd.user.services.mpvpaper-autostart = {
+        #         Unit = {
+        #                 Description = "Autostart mpvpaper.";
+        #                 PartOF = [ "graphical-session.target" ];
+        #                 After = [ "graphical-session.target" ];
+        #         };
+        #         Install = {
+        #                 WantedBy = [ "graphical-session.target" ];
+        #         };
+        #         Service = {
+        #                 ExecStart = "${pkgs.writeShellScript "mpvpaper-autostart" ''
+        #                         #!/run/current-system/sw/bin/bash
+        #                         ${pkgs.mpvpaper}/bin/mpvpaper -o "no-audio loop" eDP-1 /home/amida/Pictures/gyr_up.mp4
+        #                 ''}";
+        #                 Restart = "on-failure";
+        #         };
+        # };
 }
