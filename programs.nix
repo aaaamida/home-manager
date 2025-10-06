721cffef564a3a4432a3a80e40d79c2ff29518e8
amida <aaaamida6747@gmail.com>
2025-10-06 15:59:03 +0800

    update: programs, shell config

--- programs.nix
+++ programs.nix
@@ -16,6 +16,19 @@
                         theme = "refined";
                 };
 
+                plugins = [
+                        {
+                                name = "nix-shell";
+                                file = "nix-shell.plugin.zsh";
+                                src = pkgs.fetchFromGitHub {
+                                        owner = "chisui";
+                                        repo = "zsh-nix-shell";
+                                        rev = "v0.8.0";
+                                        sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+                                };
+                        }
+                ];
+
                 shellAliases = {
                         ls = "eza -laTL 2 --git --git-repos --icons=always --hyperlink ";
                         cat = "bat";
@@ -30,6 +43,7 @@
                         ngc = "nix-collect-garbage";
                         open = "xdg-open";
                         cnf = "command-not-found";
+                        inx = "echo $IN_NIX_SHELL";
                 };
 
                 initContent = ''
@@ -70,6 +84,13 @@
         programs.helix = {
                 enable = true;
                 extraPackages = with pkgs; [ nil ];
+        };
+
+        programs.direnv = {
+                enable = true;
+                enableZshIntegration = true;
+                enableBashIntegration = true;
+                nix-direnv.enable = true;
         };
 
         programs.zed-editor.enable = true;
